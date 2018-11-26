a6e87b1734e739f1a41d26faa8bf851337843c26
deehefka <marhefkadas@gmail.com>
2018-11-26 14:11:28 -0500

    API log in/log out
    sign up, sign in, change pass and sign out

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const store = require('../store.js')
+
+const signUpSuccess = data => {
+  $('#message').text('You signed up! Please sign in!')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  // clears sign up information
+  $('#sign-up').trigger('reset')
+  // console.log('signUpSuccess ran. Data is :', data)
+  $('#sign-up').hide()
+}
+
+const signUpFailure = data => {
+  $('#message').text('Error on sign up')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  // clears sign up information
+  $('#sign-up').trigger('reset')
+  // console.error('signUpFailure ran. Error is :', error)
+}
+
+const signInSuccess = data => {
+  store.user = data.user
+  $('#sign-up').hide()
+  $('#sign-in').hide()
+  document.getElementById('change-password').hidden = false
+  document.getElementById('sign-out').hidden = false
+  $('#message').text('You signed in! Create some To-Dos!')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  // console.log('signInSuccess ran. Data is :', data)
+  $('#sign-in').trigger('reset')
+}
+
+const signInFailure = data => {
+  $('#message').text('Error on sign in')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  $('#sign-in').trigger('reset')
+  // console.error('signInFailure ran. Error is :', error)
+}
+
+const changePasswordSuccess = data => {
+  $('#message').text('You changed your password!')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  // console.log('changePasswordSuccess ran. Data is :', data)
+  $('#change-password').trigger('reset')
+}
+
+const changePasswordFailure = data => {
+  $('#message').text('Error on password change')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  // console.error('changePasswordFailure ran. Error is :', error)
+  $('#change-password').trigger('reset')
+}
+
+const signOutSuccess = data => {
+  $('#message').text('Bye now!')
+  store.user = null
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  // console.log('signOutSuccess ran. Data is :', data)
+}
+
+const signOutFailure = data => {
+  $('#message').text('Error on sign out')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  // console.error('signOutFailure ran. Error is :', error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://to-do-project-2.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
