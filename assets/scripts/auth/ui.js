bfd84ae51a14c511afbf34898e7e9f91f3942559
deehefka <marhefkadas@gmail.com>
2018-12-07 10:32:57 -0500

    Buttons after log out and log in
    get to show after log out/log in? Change pass and sign out

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -25,14 +25,16 @@ const signInSuccess = data => {
   store.user = data.user
   $('#sign-up').hide()
   $('#sign-in').hide()
+  $('#change-password').show()
+  $('#sign-out').show()
   // hiding or showing elements after click
-  document.getElementById('change-password').hidden = false
+  // document.getElementById('change-password').hidden = false
   document.getElementById('my-to-dos').hidden = false
   document.getElementById('todo_list-create').hidden = false
   document.getElementById('todo_list-update').hidden = false
   document.getElementById('todo_list-index').hidden = false
   document.getElementById('todo_list-delete').hidden = false
-  document.getElementById('sign-out').hidden = false
+  // document.getElementById('sign-out').hidden = false
   $('#message').text('You signed in! Create some To-Dos!')
   $('#message').removeClass()
   $('#message').addClass('success')
